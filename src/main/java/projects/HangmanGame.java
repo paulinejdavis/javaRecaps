0a0599a7c5f62c3e1c759d5dd40272ba174ce15f
pauline <pauljdavi.t21@btinternet.com>
2025-06-23 10:32:28 +0100

    add functional projects

---
+++ src/main/java/projects/HangmanGame.java
@@ -0,0 +1,8 @@
+package projects;
+
+public class HangmanGame {
+    public static void main(String[] args) {
+        Hangman hangman = new Hangman("olives", 2);
+        hangman.play();
+    }
+}
---
+++ src/main/java/projects/TicTacToe.java
@@ -0,0 +1,10 @@
+package projects;
+
+public class TicTacToe {
+    private char[][] board;
+    private char currentPlayer;
+
+    public TicTacToe() {
+
+    }
+}
